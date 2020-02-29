0ac3a365b8542107fa781f96d312c0e62f07b284
YuLing Liu <yuling.liu@thoughtworks.com>
2020-02-29 21:35:41 +0800

    Change condition to switch

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -21,29 +21,30 @@ class GildedRose {
     }
 
     private void updatedQuality(Item item) {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")
-        ) {
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        switch (item.name) {
+            case "Sulfuras, Hand of Ragnaros":
+               return;
+            case "Aged Brie":
+                if (item.sell_in < 0 && item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+                return;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                    updatedBackstageRoseQuality(item);
+                }
+                return;
+            default:
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
                     if (item.sell_in < 0) {
                         item.quality = item.quality - 1;
                     }
                 }
-            }
-        } else {
-            if (item.name.equals("Aged Brie")) {
-                if (item.sell_in < 0 && item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    updatedBackstageRoseQuality(item);
-                }
-            }
         }
     }
 
@@ -64,6 +65,5 @@ class GildedRose {
         if (item.sell_in < 0) {
             item.quality = item.quality - item.quality;
         }
-
     }
 }
