66177eb1809a467288dce41effc2e10b3bb31747
YuLing Liu <yuling.liu@thoughtworks.com>
2020-02-29 18:35:18 +0800

    slide statements

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -31,43 +31,44 @@ class GildedRose {
             }
 
             if (item.sell_in < 0) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
                     }
+                }
             }
         } else {
-            if(item.name.equals("Aged Brie")){
+            if (item.name.equals("Aged Brie")) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
             }
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
-
-                updatedBackstageRoseQuality(item);
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    updatedBackstageRoseQuality(item);
+                }
             }
         }
     }
 
     private void updatedBackstageRoseQuality(Item item) {
-        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.sell_in < 11) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
 
-            if (item.sell_in < 6) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-
-            if (item.sell_in < 0) {
-                item.quality = item.quality - item.quality;
+        if (item.sell_in < 11) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
             }
         }
+
+        if (item.sell_in < 6) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+
+        if (item.sell_in < 0) {
+            item.quality = item.quality - item.quality;
+        }
+
     }
 }
