430e352cb2d88ccd71e6532b81cb7943c6a33084
YuLing Liu <yuling.liu@thoughtworks.com>
2020-02-29 21:13:03 +0800

    slide statements

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -27,8 +27,6 @@ class GildedRose {
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
-                }
-                if (item.quality > 0) {
                     if (item.sell_in < 0) {
                         item.quality = item.quality - 1;
                     }
